081c23cc3fa9f6a189f278ce4bb210c8743c7760
Naoto Kaneko <naoty.k@gmail.com>
2013-07-23 01:01:20 +0900

    イヤホンジャックの抜き差しを検知する

--- HeadphoneJackSample/HJViewController.m
+++ HeadphoneJackSample/HJViewController.m
@@ -7,6 +7,18 @@
 //
 
 #import "HJViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import <AudioToolbox/AudioToolbox.h>
+
+static void audioRouteChangeListenerCallback(void *inUserData, AudioSessionPropertyID inPropertyID, UInt32 inPropertyValueSize, const void *inPropertyData)
+{
+    NSLog(@"audioRouteChangeListenerCallback");
+}
+
+static void recordingCallback(void *inUserData, AudioQueueRef inAQ, AudioQueueBufferRef inBuffer, const AudioTimeStamp *inStartTime, UInt32 inNumberPacketDescriptions, const AudioStreamPacketDescription *inPacketDescs)
+{
+    NSLog(@"recordingCallback");
+}
 
 @interface HJViewController ()
 
@@ -17,13 +29,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    // オーディオセッションの初期化
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    
+    // 録音用カテゴリの設定
+    NSError *setCategoryError = nil;
+    BOOL setCategorySuccess = [session setCategory:AVAudioSessionCategoryRecord error:&setCategoryError];
+    if (setCategorySuccess) {
+        NSLog(@"Successfully set category.");
+    } else {
+        NSLog(@"Failed to set category: %@", setCategoryError);
+    }
+    
+    // オーディオの経路変化を検知する
+    AudioSessionPropertyID routeChangeID = kAudioSessionProperty_AudioRouteChange;
+    AudioSessionAddPropertyListener(routeChangeID, audioRouteChangeListenerCallback, NULL);
+    
+    // オーディオセッションのアクティブ化
+    NSError *activationError = nil;
+    BOOL activationSuccess = [session setActive:YES error:&activationError];
+    if (activationSuccess) {
+        NSLog(@"Successfully activated.");
+    } else {
+        NSLog(@"Failed to activate: %@", activationError);
+    }
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
